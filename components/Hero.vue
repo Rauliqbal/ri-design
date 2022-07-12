d7b930117f22f37edc938ded10493c8628ebbfcb
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-07-12 14:03:29 +0700

    first commit

---
+++ components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center min-h-screen flex flex-col justify-center gap-4 md:gap-8">
+    <h1 class="text-4xl md:text-6xl">Creative <span class="text-gradient font-semibold">design</span> <br> for a your project.</h1>
+    <p class="text-base md:text-2xl">We provide ready-to-use templates <br> for your awesome projects.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <hero/>
+  </div>
+</template>
+
+<script>
+import Hero from "~/components/Hero.vue"
+
+export default {
+   components: { Hero }, 
+  name: 'IndexPage' 
+}
+</script>
